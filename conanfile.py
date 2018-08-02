2c3bd14207403a44995b1cd93fd70f46d37abdc2
Jeroen Koekkoek <jeroen.koekkoek@adlinktech.com>
2018-08-02 13:56:39 +0200

    Update recipe

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class CriterionConan(ConanFile):
     name = "criterion"
     version = "2.3.2"
     description = "A cross-platform C and C++ unit testing framework for the 21th century"
-    url = "https://github.com/k0ekk0ek/conan-criterion"
+    url = "https://github.com/atolab/conan-criterion"
     homepage = "https://github.com/Snaipe/Criterion"
     license = "MIT"
     exports = ["LICENSE.md"]
@@ -27,13 +27,13 @@ class CriterionConan(ConanFile):
     build_subfolder = "build_subfolder"
 
     requires = (
-        'nanomsg/06252016@k0ekk0ek/stable',
+        ('nanomsg/06252016@k0ekk0ek/stable', 'private'),
         # FIXME: Should be handled in the same way as Klib as only smalloc is
         #        used by Criterion and libcsptr is probably not worth being
         #        maintained as a separate package.
-        'libcsptr/2.0.4@k0ekk0ek/stable',
-        'dyncall/09132016@k0ekk0ek/stable',
-        'boxfort/12122016@k0ekk0ek/stable'
+        ('libcsptr/2.0.4@k0ekk0ek/stable', 'private'),
+        ('dyncall/09132016@k0ekk0ek/stable', 'private'),
+        ('boxfort/12122016@k0ekk0ek/stable', 'private')
     )
 
     branch = "master"
@@ -45,7 +45,7 @@ class CriterionConan(ConanFile):
 
     def requirements(self):
         if self.settings.os == 'Windows':
-            self.requires('wingetopt/0.95@k0ekk0ek/stable')
+            self.requires('wingetopt/0.95@k0ekk0ek/stable', private=True, override=False)
 
     def source(self):
         self.run('git clone --branch={0} {1}.git {2}'
